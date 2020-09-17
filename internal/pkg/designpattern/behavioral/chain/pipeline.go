257c2ccd6220f50af0a24234ce1c4b41cdf70394
kevin_789 <kevin_789@126.com>
2020-09-17 09:49:39 +0800

    feat: update + simple Chain of Responsibility pattern

---
+++ internal/pkg/designpattern/behavioral/chain/pipeline.go
@@ -0,0 +1,74 @@
+package chain
+
+import (
+	"fmt"
+)
+
+type Request struct {
+	tagId string
+}
+
+func (request *Request) SetTagId(tagId string) {
+	request.tagId = tagId
+}
+
+func (request *Request) GetTagId() string {
+	return request.tagId
+}
+
+type Response struct {
+}
+
+/**
+interface
+*/
+type Handler interface {
+	DoHandler(request Request, response Response)
+}
+
+type Pipeline interface {
+	AddLast(handler Handler)
+
+	DoHandler(request Request, response Response)
+}
+
+/**
+implement
+*/
+type PipelineImpl struct {
+	handlers []Handler
+}
+
+func (pipeline *PipelineImpl) AddLast(handler Handler) {
+	var hs []Handler
+	hs = append(hs, handler)
+	if pipeline.handlers == nil || len(pipeline.handlers) < 1 {
+		pipeline.handlers = []Handler{}
+	}
+	pipeline.handlers = append(pipeline.handlers, handler)
+}
+
+func (pipeline *PipelineImpl) DoHandler(request Request, response Response) {
+	for index, handler := range pipeline.handlers {
+		fmt.Println("handler index: ", index, "->", &handler)
+		handler.DoHandler(request, response)
+	}
+}
+
+/**
+metrics handler
+*/
+type MetricsHandler struct{}
+
+func (handler *MetricsHandler) DoHandler(request Request, response Response) {
+	fmt.Println("metrics", "->", request.tagId)
+}
+
+/**
+global handler
+*/
+type GlobalHandler struct{}
+
+func (handler *GlobalHandler) DoHandler(request Request, response Response) {
+	fmt.Println("global", "->", request.tagId)
+}
---
+++ internal/pkg/designpattern/behavioral/chain/pipeline_test.go
@@ -0,0 +1,33 @@
+package chain
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPipeline(t *testing.T) {
+	request := new(Request)
+	request.SetTagId("test")
+
+	tagId := request.GetTagId()
+
+	fmt.Println(tagId)
+	fmt.Println(request.GetTagId())
+}
+
+func TestPipelineImpl(t *testing.T) {
+
+	pip := new(PipelineImpl)
+	metricsHandler := new(MetricsHandler)
+	globalHandler := new(GlobalHandler)
+
+	pip.AddLast(metricsHandler)
+	pip.AddLast(globalHandler)
+
+	request := new(Request)
+	request.SetTagId("test-xxx")
+
+	response := new(Response)
+
+	pip.DoHandler(*request, *response)
+}
